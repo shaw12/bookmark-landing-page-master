746cfe5ef5a2dd90dd702ee9cf5013fe6799c42a
shaw12 <hussainmohdyasir@gmail.com>
2020-06-21 16:20:35 +0530

    test commit

--- src/acc.js
+++ src/acc.js
@@ -1,50 +1,88 @@
 $(document).ready(function(){
+
   $('#accordion-content-1').hide();
+  $('#accordion-content-2').hide();
+  $('#accordion-content-3').hide();
+  $('#accordion-content-4').hide();
+  $('#down').hide();
+  $('#down2').hide();
+  $('#down3').hide();
+  $('#down4').hide();
+
+  $('#accordion-button-1').on('click', (event) => {
+    $('#accordion-content-1').toggle();
     $('#accordion-content-2').hide();
     $('#accordion-content-3').hide();
     $('#accordion-content-4').hide();
-  $('#accordion-button-1').on('click', () => {
-    $('#accordion-content-1').show();
-    
-    $('#accordion-content-2').hide();
-    $('#accordion-content-3').hide();
-    $('#accordion-content-4').hide();
+    $('#down').toggle();
+    $('#up').toggle();
+    $('#down').addClass('fa');
+    $('#down2').hide();
+    $('#up2').show();
+    $('#down3').hide();
+    $('#up3').show();
+    $('#down4').hide();
+    $('#up4').show();
   });
   $('#accordion-button-2').on('click', () => {
-    $('#accordion-content-2').show();
+    $('#accordion-content-2').toggle();
     $('#accordion-content-1').hide();
     $('#accordion-content-3').hide();
     $('#accordion-content-4').hide();
+    $('#down2').toggle();
+    $('#up2').toggle();
+    $('#down2').addClass('fa');
+    $('#down').hide();
+    $('#up').show();
+    $('#down3').hide();
+    $('#up3').show();
+    $('#down4').hide();
+    $('#up4').show();
   });
   $('#accordion-button-3').on('click', () => {
-    $('#accordion-content-3').show();
+    $('#accordion-content-3').toggle();
     $('#accordion-content-2').hide();
     $('#accordion-content-1').hide();
     $('#accordion-content-4').hide();
+    $('#down3').toggle();
+    $('#up3').toggle();
+    $('#down3').addClass('fa');
+    $('#down2').hide();
+    $('#up2').show();
+    $('#down').hide();
+    $('#up').show();
+    $('#down4').hide();
+    $('#up4').show();
   });
   $('#accordion-button-4').on('click', () => {
-    $('#accordion-content-4').show();
+    $('#accordion-content-4').toggle();
     $('#accordion-content-2').hide();
     $('#accordion-content-3').hide();
     $('#accordion-content-1').hide();
-    
+    $('#down4').toggle();
+    $('#up4').toggle();
+    $('#down4').addClass('fa');
+    $('#down2').hide();
+    $('#up2').show();
+    $('#down3').hide();
+    $('#up3').show();
+    $('#down').hide();
+    $('#up').show();
   });
-})
-
-
-/*const items = document.querySelectorAll(".accordion button");
-
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
-
-items.forEach(item => item.addEventListener('click', toggleAccordion))
-*/
+  $('.ride1').addClass('activex');
+  $('.ride1').on('click', () => {
+    $('.ride1').addClass('activex');
+    $('.ride2').removeClass('activex');
+    $('.ride3').removeClass('activex');
+  })
+  $('.ride2').on('click', () => {
+    $('.ride2').addClass('activex');
+    $('.ride1').removeClass('activex');
+    $('.ride3').removeClass('activex');
+  })
+  $('.ride3').on('click', () => {
+    $('.ride3').addClass('activex');
+    $('.ride2').removeClass('activex');
+    $('.ride1').removeClass('activex');
+  })
+});
